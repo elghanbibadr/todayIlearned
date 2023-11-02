47044913001cafd4c853b1d264533a74eabf7a54
badr ghanbi <bghanbi50@gmail.com>
2023-11-02 15:18:35 +0100

    created the buttons componenets

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import styled from "styled-components";
+{
+  /* <Button text="SCIENCE">SCIENCE</Button> */
+}
 
-const Button = () => {
-  return <div>Button</div>;
-};
+const Button = styled.button`
+  color: var(--darkWhite);
+  border-radius: 30px;
+  padding: 6px 40px;
+  font-size: 1.8rem;
+  margin-block: 10px;
+  border: none;
+  ${(props) =>
+    props.text === "FINANCE" &&
+    `
+    background-color: var(--red);
+  `}
+  ${(props) =>
+    props.text === "TECHNOLOGY" &&
+    `
+    background-color: var(--blue);
+  `}
+  ${(props) =>
+    props.text === "HISTORY" &&
+    `
+    background-color: var(--orange);
+  `}
+  ${(props) =>
+    props.text === "SCIENCE" &&
+    `
+    background-color: var(--green);
+  `}
+  ${(props) =>
+    props.text === "ENTERTAINMENT" &&
+    `
+    background-color: var(--pink);
+  `};
+`;
 
 export default Button;
